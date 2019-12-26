e45cd54bbd831ed359ea8c63280c261260931135
David Caspar <david.caspar@aol.ch>
2019-12-26 23:52:40 +0100

    Fixed a few issues with the application startup.

--- taskbutler-ui-vaadin-app/src/main/kotlin/ch/d4span/taskbutler/ui/vaadin/TaskbutlerVaadinApplication.kt
+++ taskbutler-ui-vaadin-app/src/main/kotlin/ch/d4span/taskbutler/ui/vaadin/TaskbutlerVaadinApplication.kt
@@ -3,7 +3,7 @@ package ch.d4span.taskbutler.ui.vaadin
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["ch.d4span.taskbutler"])
 open class TaskbutlerVaadinApplication
 
 fun main(args: Array<String>) {
--- taskbutler-ui-vaadin-frameworks-drivers/taskbutler-ui-vaadin-frontend/src/main/kotlin/ch/d4span/taskbutler/ui/vaadin/views/MainView.kt
+++ taskbutler-ui-vaadin-frameworks-drivers/taskbutler-ui-vaadin-frontend/src/main/kotlin/ch/d4span/taskbutler/ui/vaadin/views/MainView.kt
@@ -1,9 +1,7 @@
 package ch.d4span.taskbutler.ui.vaadin.views
 
-import com.vaadin.flow.component.Text
 import com.vaadin.flow.component.orderedlayout.FlexComponent
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
-import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.tabs.Tab
 import com.vaadin.flow.component.tabs.Tabs
 import com.vaadin.flow.router.Route
